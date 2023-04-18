ee5c2919609f36d9ca288edbcff802935ffa7aa3
stopbitch2442 <t.khmelevv@yandex.ru>
2023-04-18 23:51:42 +0300

    Добавьте файлы проекта.

---
+++ MagicFactory/Details.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MagicFactory
+{
+    public class MeansOfTransportation // средство передвижения транспорта
+    {
+        public enum TransportationName
+        {
+            Wheels,
+            Tracks,
+            Blades,
+            Rails,
+            Chassis
+        }
+        public string Name { get; set; }
+
+        public MeansOfTransportation(string name)
+        {
+            Name = name;
+        }
+
+    }
+
+    public class MachineBody // корпус транспорта
+    {
+        public enum BodyName
+        {
+            Car,
+            Tank,
+            Boat,
+            Train,
+            Plane,
+            Helicopter
+        }
+        public string Name { get; set; }
+
+        public MachineBody(string name) 
+        {
+            Name = name;
+        }
+
+    }
+
+    public class Transport
+    {
+        readonly Random rnd = new Random();
+     
+    }
+
+
+}
