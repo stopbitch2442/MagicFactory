acf8e0703ba5324e57b6823857a54206cd26a38e
stopbitch2442 <t.khmelevv@yandex.ru>
2023-04-19 18:11:28 +0300

    19.04.2023 18.11 Magic_functionally

---
+++ MagicFactory/Command.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MagicFactory
+{
+    public class Command
+    {
+        public int Value { get; set; }
+        public string Description { get; set; }
+
+        public Command RandomDetails = new Command { Value = 1, Description = "Собрать транспорт из рандомных деталей" };
+        public Command SelectedDetails = new Command { Value = 2, Description = "Выбрать детали для нашей машины" };
+    }
+}
--- MagicFactory/Details.cs
+++ MagicFactory/Details.cs
@@ -1,55 +1,87 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace MagicFactory
 {
+    public enum BodyName
+    {
+        Car,
+        Tank,
+        Boat,
+        Train,
+        Plane,
+        Helicopter
+    }
+
+    public enum TransportationName
+    {
+        Tracks,
+        Wheels,
+        Blades,
+        Rails,
+        Chassis
+    }
     public class MeansOfTransportation // средство передвижения транспорта
     {
-        public enum TransportationName
-        {
-            Wheels,
-            Tracks,
-            Blades,
-            Rails,
-            Chassis
-        }
-        public string Name { get; set; }
-
-        public MeansOfTransportation(string name)
-        {
-            Name = name;
-        }
+       
 
     }
 
     public class MachineBody // корпус транспорта
     {
-        public enum BodyName
-        {
-            Car,
-            Tank,
-            Boat,
-            Train,
-            Plane,
-            Helicopter
-        }
-        public string Name { get; set; }
-
-        public MachineBody(string name) 
-        {
-            Name = name;
-        }
-
+        
+      
     }
 
     public class Transport
     {
-        readonly Random rnd = new Random();
-     
+        
+
+        private readonly List<string> _outputStrings;
+
+        public  Transport(List<string> outputStrings)
+        {
+            _outputStrings = outputStrings;
+        }
+        public static void TransportCreate()
+        {
+            Console.WriteLine("Собираем нашу чудо технику:");
+            Console.WriteLine("1) Собрать рандом, проверить");
+            Console.WriteLine("2) Выбрать детали, вывести список что получилось, проверить");
+
+            Random random = new Random();
+
+            Array bodyName = Enum.GetValues(typeof(BodyName));
+            BodyName randomBody = (BodyName)bodyName.GetValue(random.Next(bodyName.Length));
+
+            Array transportationName = Enum.GetValues(typeof(TransportationName));
+            TransportationName randomTransportation = (TransportationName)transportationName.GetValue(random.Next(transportationName.Length));
+
+            Console.WriteLine($"Корпус: {randomBody}");
+            Console.WriteLine($"Средство передвижения: {randomTransportation}");
+
+        //    TestMagicMachine(randomBody, randomTransportation);
+        }
+
     }
 
 
 }
+
+
+//Вывод на консоль:
+//Собираем нашу чудо технику:
+//1) Собрать рандом, проверить
+//2) Выбрать детали, вывести список что получилось, проверить
+
+
+//проверить технику  - метод TestMagicMachine (определённые параметры присущие виду)
+
+//детали - колёса, основа, башня(пушка)
+
+//средства движения - гусеницы, колёса, лопасти, рельсы, шасси
+//корпус - машина, танк, катер, поезд, самолёт, вертолёт
