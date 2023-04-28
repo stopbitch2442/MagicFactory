94ad0a447680d7ad3538a3cfbdb26001493c355d
stopbitch2442 <t.khmelevv@yandex.ru>
2023-04-28 18:36:13 +0300

    GasPedal_EventPress_IncreaseEngineSpeed Complete Test

--- MagicFactory.Tests/TransportValidationTests.cs
+++ MagicFactory.Tests/TransportValidationTests.cs
@@ -2,6 +2,8 @@
 using MagicFactory.Car;
 using MagicFactory;
 using static MagicFactory.Car.Suspention;
+using static MagicFactory.Car.Engine.ElectricEngine;
+using static MagicFactory.Car.Car;
 
 namespace MagicFactory.Tests
 {
@@ -28,5 +30,37 @@ namespace MagicFactory.Tests
                 Assert.Equal(30, wheel.Speed);
             }
         }
+
+        [Fact]
+        public void ChangeDumperPosition_TestValue()
+        {
+            //arrange
+            var engine = new TeslaEngine();
+            //act
+            int changeResult = engine.ChangeDumperPosition(100);
+            //assert
+            Assert.Equal(111, changeResult);
+        }
+
+        [Fact]
+        public void GasPedal_EventPress_IncreaseEngineSpeed()
+        {
+            //arrange
+            Pedals[] pedals = new Pedals [] { new GasPedal(), new BrakePedal()};
+            var suspention = new TeslaSuspention();
+            var engine = new TeslaEngine();
+            var registrationNumber = "Р131ТЕ40";
+            Car.Car car = new Tesla(pedals, suspention, engine, registrationNumber);
+
+            double startSpeed = engine.Speed;   //Speed = 0
+            //act
+            car.GasPedal_EventPress(this, EventArgs.Empty);
+            double afterSpeed = engine.Speed;
+            //assert
+            Assert.True(startSpeed < afterSpeed);
+        }
+
+
+
     }
 }
